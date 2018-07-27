5d5d45587984fec276ba4b74b50bc51ad3d6b84c
Daniel Breitigan <danthemanb3@gmail.com>
2018-07-27 10:38:15 -0600

    Add controller test

---
+++ src/test/java/com/example/demo/controller/WordControllerTest.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.WordService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(WordController.class)
+public class WordControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    private WordService service;
+
+    @Test
+    public void testWordCount() throws Exception {
+        String request = "this is a test string";
+
+        Map<String, Integer> map = new HashMap<>();
+        map.put("this", 1);
+        map.put("is", 1);
+        map.put("a", 1);
+        map.put("test", 1);
+        map.put("string", 1);
+
+        when(service.wordCounter(request)).thenReturn(map);
+
+        this.mvc.perform(post("/word/count")
+                .content(request))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.this", is(1)))
+                .andExpect(jsonPath("$.is", is(1)))
+                .andExpect(jsonPath("$.a", is(1)))
+                .andExpect(jsonPath("$.test", is(1)))
+                .andExpect(jsonPath("$.string", is(1)));
+    }
+
+}
